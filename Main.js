8bcc0f72f01748f8fe014b462b6f6b9326b4fefa
logesh-001 <logeshwaranshanmugam02@gmail.com>
2024-02-16 15:02:12 +0530

    Initial commit

---
+++ Main.js
@@ -0,0 +1,14 @@
+const togglebtn = document.querySelectorAll('.toggle_btn');
+ const togglebtIcon = document.querySelectorAll('.toggle_btn i'); 
+const dropdown = document.querySelectorAll('.dropdown_menu');
+
+togglebtn.onclick=function (){
+    dropdown.classList.contains('open')
+
+    togglebtIcon.classList=isOpen
+    ?'fa-solid fa-xmark'
+    :'fa-solid fa-bars'
+}
+function togglebtn(){
+    
+}
